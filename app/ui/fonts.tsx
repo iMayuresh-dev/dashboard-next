816827c6ea01018b560c07c6f726a2992c51ec17
MayureshGavai <blackoutmayuresh2912@gmail.com>
2025-02-16 23:00:24 +0530

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,14 @@
+import './global.css';
+
+import {Inter, Lusitana} from 'next/font/google';
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400'
+})
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+})
